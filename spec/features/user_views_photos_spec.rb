9c94c0bbc8d0806ee3c9638e5e90ade1601c4f41
Ted <tedkwasnik@gmail.com>
2014-07-07 14:44:52 -0400

    failing add photos to map test

--- spec/features/user_views_photos_spec.rb
+++ spec/features/user_views_photos_spec.rb
@@ -17,6 +17,8 @@ so I can manage them
     photo = FactoryGirl.create(:photo, user_id: user.id)
     photo2 =  FactoryGirl.create(:photo, user_id: user.id, image:  Rack::Test::UploadedFile.new(File.join(Rails.root, 'app', 'assets', 'images', 'test2.jpg'))  )
     photo3 = FactoryGirl.create(:photo, geom: nil, user_id: user.id, image:  Rack::Test::UploadedFile.new(File.join(Rails.root, 'app', 'assets', 'images', 'unmap1.jpg')) )
+    user_my_map = FactoryGirl.create(:my_map, user: user, name: "My First Map")
+
 
     user2 = FactoryGirl.create(:user)
     photo4 =  FactoryGirl.create(:photo, user_id: user2.id, image:  Rack::Test::UploadedFile.new(File.join(Rails.root, 'app', 'assets', 'images', 'test2.jpg')) )
@@ -49,6 +51,21 @@ so I can manage them
         markers = all(".leaflet-marker-icon")
         markers[1].click
         expect( all(".selected_photo_div").count ).to eq(2)
+    end
+
+    scenario 'user can add selection to existing map', js: true do
+        sign_in_as(user)
+
+        all(".leaflet-marker-icon")[0].click
+        all(".leaflet-marker-icon")[1].click
+        select "My First Map", from: "Add To Map"
+        click_on "Submit"
+
+        click_on "My First Map"
+        expect(page).to have_content "My First Map"
+        expect( all(".c_map_content").count ).to  eq(2)
+        expect( match_url(page.find("#photo#{photo.id}")['src'], photo.image_url )).to eq(true)
+        expect( match_url(page.find("#photo#{photo2.id}")['src'], photos2.image_url )).to eq(true)
     end
 end
 
