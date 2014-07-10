b97cbbc6d814779831f8e72edbbe77d64d4be133
Ted <tedkwasnik@gmail.com>
2014-07-10 13:35:35 -0400

    passing upload tests - will swtich to ajax

--- spec/features/user_manages_photos_spec.rb
+++ spec/features/user_manages_photos_spec.rb
@@ -20,14 +20,14 @@ so I can manage them
 
     user = FactoryGirl.create(:user, email: "test@tets.com")
     photo = FactoryGirl.create(:photo, user_id: user.id)
-    photo2 =  FactoryGirl.create(:photo, user_id: user.id, image:  Rack::Test::UploadedFile.new(File.join(Rails.root, 'app', 'assets', 'images', 'test2.jpg'))  )
-    photo3 = FactoryGirl.create(:photo, geom: nil, user_id: user.id, image:  Rack::Test::UploadedFile.new(File.join(Rails.root, 'app', 'assets', 'images', 'unmap1.jpg')) )
+    photo2 =  FactoryGirl.create(:photo, user_id: user.id, image:  Rack::Test::UploadedFile.new( File.join(Rails.root, 'app', 'assets', 'images', 'test2.jpg'))  )
+    photo3 = FactoryGirl.create(:photo, geom: nil, user_id: user.id, image:  Rack::Test::UploadedFile.new( File.join(Rails.root, 'app', 'assets', 'images', 'unmap1.jpg')) )
     user_my_map = FactoryGirl.create(:my_map, user: user, name: "My First Map")
 
 
     user2 = FactoryGirl.create(:user)
-    photo4 =  FactoryGirl.create(:photo, user_id: user2.id, image:  Rack::Test::UploadedFile.new(File.join(Rails.root, 'app', 'assets', 'images', 'test2.jpg')) )
-    photo5 = FactoryGirl.create(:photo, geom: nil, user_id: user2.id, image:  Rack::Test::UploadedFile.new(File.join(Rails.root, 'app', 'assets', 'images', 'unmap1.jpg')) )
+    photo4 =  FactoryGirl.create(:photo, user_id: user2.id, image:  Rack::Test::UploadedFile.new( File.join(Rails.root, 'app', 'assets', 'images', 'test2.jpg')) )
+    photo5 = FactoryGirl.create(:photo, geom: nil, user_id: user2.id, image:  Rack::Test::UploadedFile.new( File.join(Rails.root, 'app', 'assets', 'images', 'unmap1.jpg')) )
 
     scenario 'must log in to see photos'do
         visit photos_path
@@ -116,5 +116,16 @@ so I can manage them
         expect( match_url(page.find("#photo#{photo.id}")['src'], photo.image_url )).to eq(true)
         expect( match_url(page.find("#photo#{photo2.id}")['src'], photo2.image_url )).to eq(true)
     end
+
+     scenario 'user can make selection of one or more photos and add them to a map',js: true do
+        sign_in_as(user)
+        count = all(".leaflet-marker-icon").count
+        page.attach_file "photo[image][]", ["#{Rails.root}/app/assets/images/test1.jpg", "#{Rails.root}/app/assets/images/test2.jpg"]
+        all("#MapNewPhotos")[0].trigger('click')
+
+        sleep(20)
+        expect( all(".leaflet-marker-icon").count ).to eq( count + 2 )
+    end
+
 end
 
