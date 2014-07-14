9f763687cb9c3bad75338e2f1c1c633675bb5bbc
Ted <tedkwasnik@gmail.com>
2014-07-13 22:13:07 -0400

    added tags to photo details

--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -1,9 +1,32 @@
 class PhotoSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :image, :placename, :related_maps
+  attributes :id, :name, :description, :image, :placename, :related_maps, :people, :activities, :tags
 
   def description
     description = object.description
     description = " " if object.description.nil?
     description
   end
+
+  def people
+    people = Category.where(name: "people")
+    tags = object.tags.where(category: people)
+    output = tags.map { |tag| tag.name }
+    output.join ", "
+  end
+
+  def activities
+    activities = Category.where(name: "activities")
+    tags = object.tags.where(category: activities)
+    output = tags.map { |tag| tag.name }
+    output.join ", "
+  end
+
+  def tags
+    other = Category.where(name: "other")
+    tags = object.tags.where(category: other)
+    output = tags.map { |tag| tag.name }
+    output.join ", "
+  end
+
+
 end
