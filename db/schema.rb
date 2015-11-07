1c51f53ab35004a96b1b73e12aab653e9fca2830
Ted <tedkwasnik@gmail.com>
2015-11-07 14:52:54 -0800

    heroku database updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140712223525) do
+ActiveRecord::Schema.define(version: 20140916012457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
+
+  create_table "additional_details", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "area_skills", force: true do |t|
+    t.integer  "expertise_area_id"
+    t.integer  "skill_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "categories", force: true do |t|
     t.string   "name",       null: false
@@ -40,6 +54,44 @@ ActiveRecord::Schema.define(version: 20140712223525) do
   end
 
   add_index "countries", ["name"], :name => "index_countries_on_name", :unique => true
+
+  create_table "expertise_areas", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "jobs", force: true do |t|
+    t.string   "company"
+    t.string   "location_id"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.string   "company_logo"
+    t.string   "title"
+    t.string   "status"
+    t.text     "highlights"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "location_types", force: true do |t|
+    t.string "name"
+    t.string "description"
+  end
+
+  create_table "locations", force: true do |t|
+    t.spatial "geom",             limit: {:srid=>4326, :type=>"point", :has_z=>true, :geographic=>true}
+    t.string  "address1"
+    t.string  "address2"
+    t.string  "city"
+    t.string  "state"
+    t.string  "zip"
+    t.string  "country"
+    t.string  "name"
+    t.string  "description"
+    t.integer "location_type_id"
+  end
 
   create_table "my_map_photos", force: true do |t|
     t.integer  "my_map_id",   null: false
@@ -83,6 +135,34 @@ ActiveRecord::Schema.define(version: 20140712223525) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.time     "original_date"
+  end
+
+  create_table "schools", force: true do |t|
+    t.string   "name"
+    t.string   "location_id"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.string   "date_note"
+    t.string   "degree"
+    t.string   "status"
+    t.text     "additional_info"
+    t.string   "image_path"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "skill_specifics", force: true do |t|
+    t.integer  "skill_id"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "skills", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "states", force: true do |t|
